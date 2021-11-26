a6d94b9ef556f0d91b9188ab0a7eaf291ea4c871
your name <784936776@qq.com>
2021-11-26 20:55:49 +0800

    Merge branch 'dev' of D:\java\workplaces\StrayAnimalsManagementSystem with conflicts.

--- src/main/java/com/HexTechGDUT/service/impl/ApplicationServiceImpl.java
+++ src/main/java/com/HexTechGDUT/service/impl/ApplicationServiceImpl.java
@@ -1,7 +1,7 @@
 package com.HexTechGDUT.service.impl;
 
 import com.HexTechGDUT.dao.ApplicationMapper;
-import com.HexTechGDUT.entity.po.Application;
+import com.HexTechGDUT.po.Application;
 import com.HexTechGDUT.service.ApplicationService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -10,61 +10,39 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-/**
- * @author HexTechGDUT
- */
 @Service
 public class ApplicationServiceImpl extends ServiceImpl<ApplicationMapper, Application> implements ApplicationService {
-
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public int apply(Application application) {
-        return baseMapper.insert(application);
+    public boolean apply(Application application) {
+        return baseMapper.insert(application) != 0;
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public int update(Application application){
-        if(application.getStatus() != 0){
-            //申请已被处理,不可更改
-            return 0;
-        }
-        QueryWrapper<Application> wrapper = new QueryWrapper<>();
-        application.setStatus(0);
-        wrapper.eq("id", application.getId());
-        return baseMapper.update(application, wrapper);
-    }
-
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public int process(Application application) {
+    public boolean process(Application application) {
         QueryWrapper<Application> wrapper = new QueryWrapper<>();
         wrapper.eq("id",application.getId());
-        return baseMapper.update(application,wrapper);
+        return baseMapper.update(application,wrapper) != 0;
     }
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public int cancel(int id) {
-        QueryWrapper<Application> wrapper = new QueryWrapper<>();
-        wrapper.eq("id", id);
-        Application application = baseMapper.selectOne(wrapper);
+    public boolean cancel(String id) {
+        Application application = baseMapper.selectById(id);
         if (application.getStatus() == 0) {
-            application.setStatus(3);
-            return baseMapper.update(application, wrapper);
+            return baseMapper.deleteById(id) != 0;
         }
-        return 0;
+        return false;
     }
 
     @Override
     public Application queryApplicationById(int id) {
-        QueryWrapper<Application> wrapper = new QueryWrapper<>();
-        wrapper.eq("id", id);
-        return baseMapper.selectOne(wrapper);
+        return baseMapper.selectById(id);
     }
 
     @Override
-    public List<Application> queryApplicationByAnimalId(int animalId) {
+    public List<Application> queryApplicationByAnimalId(String animalId) {
         QueryWrapper<Application> wrapper = new QueryWrapper<>();
         wrapper.eq("animal_record_id",animalId);
         return baseMapper.selectList(wrapper);
